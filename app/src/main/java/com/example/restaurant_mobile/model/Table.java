0a6e626fac3d4b794bdf44297f0b582a5be5a91a
ßlęssed <59447184+GoranVelikov@users.noreply.github.com>
2026-02-24 14:58:06 +0200

    Update

---
+++ app/src/main/java/com/example/restaurant_mobile/model/Table.java
@@ -0,0 +1,9 @@
+package com.example.restaurant_mobile.model;
+
+public class Table {
+
+    private String id;
+    private int seats;
+    private boolean reserved;
+
+}
---
+++ app/src/main/java/com/example/restaurant_mobile/ui/dialogs/ReservationDialogFragment.java
@@ -0,0 +1,53 @@
+package com.example.restaurant_mobile.ui.dialogs;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.restaurant_mobile.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ReservationDialogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class ReservationDialogFragment extends Fragment {
+    private final static String ARG_TABLE="table_Id";
+
+
+    public ReservationDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static ReservationDialogFragment newInstance(String tableId) {
+        ReservationDialogFragment fragment = new ReservationDialogFragment();
+
+        Bundle args = new Bundle();
+        args.putString(ARG_TABLE, tableId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            String tableId = getArguments().getString(ARG_TABLE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservation_dialog, container, false);
+    }
+
+    public void show(FragmentManager supportFragmentManager, String reservationDialog) {
+
+    }
+}
